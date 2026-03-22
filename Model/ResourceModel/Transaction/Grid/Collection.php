6139b0e0cdbc5fbf3ed12ee87da22bab2341ac6e
Kenneth Madu Onah <kenneth@nanoteck.co.za>
2026-03-22 21:52:35 +0200

    Sort order for log grid

--- Model/ResourceModel/Transaction/Grid/Collection.php
+++ Model/ResourceModel/Transaction/Grid/Collection.php
@@ -6,4 +6,10 @@ use Magento\Framework\View\Element\UiComponent\DataProvider\SearchResult;
 
 class Collection extends SearchResult
 {
+    protected function _initSelect(): Collection
+    {
+        parent::_initSelect();
+        $this->setOrder('entity_id', 'DESC');
+        return $this;
+    }
 }
